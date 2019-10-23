8532b48ee528555bf94602a0317da130ccf6e2e0
Tim-Romer <Tim.Romer@gmail.com>
2019-10-23 11:49:18 -0400

    Added script to convert .shp to json file. NOT CURRENTLY WORKING

---
+++ DataAnalysis/shp_to_geojson.py
@@ -0,0 +1,18 @@
+import pyshp as shapefile
+# read the shapefile
+reader = shapefile.Reader("TigerData/tl_2017_39_tract.shp")
+fields = reader.fields[1:]
+field_names = [field[0] for field in fields]
+buffer = []
+for sr in reader.shapeRecords():
+   atr = dict(zip(field_names, sr.record))
+   geom = sr.shape.__geo_interface__
+   buffer.append(dict(type="Feature", \
+    geometry=geom, properties=atr)) 
+
+# write the GeoJSON file
+from json import dumps
+geojson = open("TigerData/tl_2017_39_tract.json", "w")
+geojson.write(dumps({"type": "FeatureCollection",\
+"features": buffer}, indent=2) + "\n")
+geojson.close()
