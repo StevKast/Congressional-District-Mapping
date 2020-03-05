b5a8c6e0e8f7f1d48e4b5269be4ea59955c1d6e6
Tim-Romer <Tim.Romer@gmail.com>
2020-03-05 10:24:08 -0500

    cleaned json merge code and organized directory

--- AlgorithmDevelopment/result_to_geo.py
+++ AlgorithmDevelopment/result_to_geo.py
@@ -9,39 +9,51 @@ import json
 dist = []
 tiger = []
 results_geoid = {}
+files = ['Lloyd/result.json', 'SplitlineDictionary.json']
 
-with open('Lloyd/result.json', 'r') as f:
-	dist = json.load(f)
+for file in files:
+	# Open the file to be read
+	with open(file, 'r') as f:
+		dist = json.load(f)
 
-with open('../DataAnalysis/TigerData/tl_2017_39_tract.json', 'r') as f:
-	tiger = json.load(f)
+	# Open the geojson file
+	with open('../DataAnalysis/TigerData/tl_2017_39_tract.json', 'r') as f:
+		tiger = json.load(f)
 	
-# Gets the geoid for each tract in each district adds it to a dictionary.
-for district in dist:
-	for i in range(len(dist[district])):
-		results_geoid[dist[district][i][0]] = district
+	# Gets the geoid for each tract in each district adds it to a dictionary.
+	if file == 'Lloyd/result.json':
+		for district in dist:
+			for i in range(len(dist[district])):
+				results_geoid[dist[district][i][0]] = district
 
 
-# Add the district information to the json
-for tract in range(len(tiger['features'])):
-	geoid = tiger["features"][tract]['properties']['GEOID']
-	tiger["features"][tract]['properties']['district'] = results_geoid[geoid]
+	# Add the district information to the json
+	for tract in range(len(tiger['features'])):
+		geoid = tiger["features"][tract]['properties']['GEOID']
+		if file == 'Lloyd/result.json':
+			tiger["features"][tract]['properties']['district'] = results_geoid[geoid]
+		else:
+			tiger["features"][tract]['properties']['district'] = 'd' + str(dist[geoid])
 
-with open('Lloyd_Result.json', 'w') as wr:
-	json.dump(tiger, wr)
+	# Write the married json to a file to json file
+	if file == 'Lloyd/result.json':
+		with open('result_Lloyd.json', 'w') as wr:
+			json.dump(tiger, wr)
+	else:
+		with open('result_' + file, 'w') as wr:
+			json.dump(tiger, wr)
 
+# with open('SplitlineDictionary.json', 'r') as f:
+# 	dist = json.load(f)
 
-with open('SplitlineDictionary.json', 'r') as f:
-	dist = json.load(f)
-
-with open('../DataAnalysis/TigerData/tl_2017_39_tract.json', 'r') as f:
-	tiger = json.load(f)
+# with open('../DataAnalysis/TigerData/tl_2017_39_tract.json', 'r') as f:
+# 	tiger = json.load(f)
 	
 
-# Add the district information to the json
-for tract in range(len(tiger['features'])):
-	geoid = tiger["features"][tract]['properties']['GEOID']
-	tiger["features"][tract]['properties']['district'] = 'd' + str(dist[geoid])
+# # Add the district information to the json
+# for tract in range(len(tiger['features'])):
+# 	geoid = tiger["features"][tract]['properties']['GEOID']
+# 	tiger["features"][tract]['properties']['district'] = 'd' + str(dist[geoid])
 
-with open('ShortestSplitLine_Result.json', 'w') as wr:
-	json.dump(tiger, wr)
+# with open('ShortestSplitLine_Result.json', 'w') as wr:
+# 	json.dump(tiger, wr)
