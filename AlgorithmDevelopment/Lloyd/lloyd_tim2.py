f6373c4d524399eeca4bb006504b5d704e620aed
Tim Romer <Tim.Romer@gmail.com>
2020-03-10 13:53:00 -0400

    started 4th iteration of lloyds implementation

--- AlgorithmDevelopment/Lloyd/lloyd_tim2.py
+++ AlgorithmDevelopment/Lloyd/lloyd_tim2.py
@@ -1,4 +1,8 @@
-import csv, os, json, numpy, random
+# File : Lloyd's Algorithm (lloyd_tim2.py) 4th iteration
+# Author : Tim Romer
+# Date : 3-10-2020
+
+import csv, os, json, numpy, random, math
 from pprint import pprint
 
 #Globals
@@ -22,8 +26,11 @@ distList = {'d1':[],
             'd14':[],
             'd15':[],
             'd16':[]}
+
+# Target population size and allowable deviation from that mean
 TARGET_DISTRICT_MEAN = 710767
 TARGET_DISTRICT_SD   = TARGET_DISTRICT_MEAN + (TARGET_DISTRICT_MEAN * 0.005)
+
 #class
 class tract:
     def __init__(self, id, pop, x, y):
@@ -45,104 +52,43 @@ class tract:
     def __repr__(self):
         return "{}, {}, {}".format(self.id, self.pop, self.coords)
 
-#Methods
-#Find current total pop of specified district
-def totalPop(key):
-    data = distList[key]
-    totalPop = 0
-    if len(data) == 0:
-        return totalPop
-    else:
-        for row in data:
-            if int(row[2]) is not None:
-                totalPop = totalPop + int(row[2])
-    return totalPop
+# ----------------------Algorithm----------------------------------------
 
+# Calculate the distance from the tract to the center
+def getDist(tract_coords, center_coords):
+	return math.sqrt(((tract_coords[0] - center_coords[0])**2) + ((tract_coords[1] - center_coords[1])**2))
 
-#Find the lowest pop distrcit
-def lowestPop():
-    lowestPopDist = 'd1'
-    for key in distList.keys():
-        if totalPop(key) < totalPop(lowestPopDist):
-            lowestPopDist = key
-    return lowestPopDist
+# Will find the closest tract to the center provided
+def find_closest_tract(X, center):
+	min_dist  = 100000000000000 # Some arbitrary value that is larger than any possible distance
+	min_tract = X[0]
+	# Loop through all of the tracts to get find the closest tract to the center
+	for x in X:
+		distance = getDist(x.getCoords(), center)
+		if distance < min_dist:
+			min_dist = distance
+			min_tract = x
+	return min_tract
 
-#Print out pop of each districts and total pop
-def printPop():
-    ohioPop = 0
-    for key in distList.keys():
-        ohioPop += totalPop(key)
-        print(key +" - "+ str(totalPop(key)))
-
-    print('ohio pop = ', ohioPop)
-
-#Transfers data from the given dict to the distList dic
-def transferData(results, data):
-    for key in results.keys(): #key -> int
-        for loc in results[key]: #loc -> numpy array
-            for d in data: #d -> list
-                if loc.getCoords()[0] == float(d[4]) and loc.getCoords()[1] == float(d[5]):
-                    currentKey = distListKeys[key]
-                    distList[currentKey].append(d)
-                    data.pop(data.index(d))
-                    break
-
-def reachCap(tractList):
-    pop = 0
-    if not tractList:
-        return false
-    else:
-        for tract in tractList:
-            pop += tract.getPop()
-    return pop > 800000
-
-#LLoyd's
-#----------------------------\/
-#input x - current data point
-#input
-def bestKey(input, mu, clusters):
-    x = input.getCoords()
-    # minimum = min([(i[0], numpy.linalg.norm(x-mu[i[0]])) for i in enumerate(mu)])[0]
-    # TIM: What is this function and what does it do?? Does it find the closest center for each tract?
-    bestkey = min([(i[0], numpy.linalg.norm(x-mu[i[0]])) for i in enumerate(mu)], key=lambda t:t[1])[0]
-    # print(clusters)
-    return bestkey
-
-# Function to make sure that all mu are still viable. Will remove from list once population exceeds mean + sd.
-# For each of the clusters, add up their total populations, if the population exceeds mu + sd, remove the district
-# from the viable mu list.
-def check_mu(viable_mu, clusters):
-    for c in clusters:
-        cluster_pop = 0
-        # print(c)
-        for index in clusters[c]:
-            cluster_pop += index.pop
-            # print(clusters[c])
-
-        if cluster_pop >= TARGET_DISTRICT_SD:
-            # print(viable_mu[c])
-            # exit()
-            viable_mu[c] = [0,0]
-            # print(viable_mu)
-            continue
-
-
-#input: data - list of all points
-#input: mu - list of current centers
-#output: dictionnary of clusters
+# This function will assign all of the tracts to a district round robin style
+# until all tracts are assigned
 def cluster_points(X, mu):
-    clusters  = {}
-    viable_mu = mu.copy()
-    for x in X:
-        bestmukey = bestKey(x, viable_mu, clusters)
-        # TIM: What is the purpose of this try catch? Why wouldn't you be able to append?
-        try:
-            clusters[bestmukey].append(x)
-        except KeyError:
-            clusters[bestmukey] = [x]
-        check_mu(viable_mu, clusters)
-    
-    return clusters
+	clusters = {} # The object being returned
+	taken_tracts = [] # Tracking the tracts that have been added
+	while len(X) != 0:
+		center_count = 0
+		for center in mu:
+			if (len(X) == 0): 
+				break
+			closest = find_closest_tract(X, center) # Will find the closest tract to that center
+			taken_tracts.append(closest)
+			X.remove(closest)
+			if center_count in clusters.keys():
+				clusters[center_count].append(closest)
+			else:
+				clusters[center_count] = [closest]
+			center_count += 1
+	return clusters
 
 #Creates new centers
 #input: mu - list of current centers
@@ -168,6 +114,7 @@ def reevaluate_centers(mu, clusters):
 def has_converged(mu, oldmu):
     return set([tuple(a) for a in mu]) == set([tuple(a) for a in oldmu])
 
+# This is a helper method to help print the cluster populations
 def print_cluster_pops(clusters):
     total_tracts = 0
     for c in clusters:
@@ -178,7 +125,6 @@ def print_cluster_pops(clusters):
         print(c, cluster_pop, len(clusters[c]))
     print(total_tracts)
     exit()    
-
 
 #Call this function for Lloyds
 #X is a list of all points, K is the number of clusters you want
@@ -192,6 +138,7 @@ def find_centers(X, K):
     mu = []
     oldmu = []
     count = 0
+    clusters = {}
 
     # TIM: get the coordinates for each tract sample and append to mu/oldmu
     for i in range(K):
@@ -201,9 +148,11 @@ def find_centers(X, K):
     # TIM: Why are we checking for convergence?
     while not has_converged(mu, oldmu):
         count += 1
+        ogX = X.copy()
         oldmu = mu
         # Assign all points in X to clusters
         clusters = cluster_points(X, mu)
+        X = ogX
         # Reevaluate centers
         mu = reevaluate_centers(oldmu, clusters)
         # print_cluster_pops(clusters)
@@ -211,8 +160,8 @@ def find_centers(X, K):
             break
         # Sanity tracker for tracking algorithm
         print(count)
+    print_cluster_pops(clusters)
     return(clusters)
-
 
 #creates list of data points based on the census tracts
 #x and y values are the coordinates
