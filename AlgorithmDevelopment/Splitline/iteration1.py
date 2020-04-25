ff17683989d272839ca8d6132177bc0602517aa6
FatherBrian <36048426+FatherBrian@users.noreply.github.com>
2020-04-25 17:59:54 -0400

    Adding Iterations to Github
    Iteration 1: Iteration.py and splitlinedictionary_iteration1.json
    
    Iteration2:iteration1.py, iteration2_ordering.py, splitlinedisctionary_iteration2.json

---
+++ AlgorithmDevelopment/Splitline/iteration1.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import json
+
+dictionary = {}
+districtNumber = 1
+tract_data = pd.read_csv("AlgorithmDevelopment_tract_data.csv")
+populations = tract_data[' POP'].tolist()
+geoID = tract_data['GEOID'].tolist()
+
+sumPop = 0
+for population in populations:
+    sumPop += int(population)
+
+
+def splitLine(tracts, geo, currentPopGoal, overallPop):
+    global districtNumber
+
+    if currentPopGoal == overallPop/16:
+        print(geo)
+        for g in geo:
+            dictionary[g] = districtNumber
+        districtNumber += 1
+        return
+
+    currentPopGoal = currentPopGoal / 2
+    sum_p = 0
+    tract_number = 0
+
+    for population_x in tracts:
+        sum_p += population_x
+        if sum_p > currentPopGoal:
+            SplitA_Pop = tracts[:tract_number]
+            SplitA_ID = geo[:tract_number]
+            SplitB_Pop = tracts[tract_number-1:]
+            SplitB_ID = geo[tract_number-1:]
+            splitLine(SplitA_Pop, SplitA_ID, currentPopGoal, overallPop)
+            splitLine(SplitB_Pop, SplitB_ID, currentPopGoal, overallPop)
+            return
+        tract_number += 1
+
+splitLine(populations, geoID, sumPop, sumPop)
+
+print(dictionary)
+
+dict_j = json.dumps(dictionary)
+
+f = open("SplitlineDictionary_iteration1.json", "w+")
+f.write(dict_j)
+f.close()
---
+++ AlgorithmDevelopment/Splitline/iteration2_ordering.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import math
+from operator import itemgetter
+
+tract_data = pd.read_csv("Lloyd/tract_data.csv")
+
+populations = tract_data[' POP'].tolist()
+geoID = tract_data['GEOID'].tolist()
+tractName = tract_data[' FULL_NAME'].tolist()
+lats = tract_data[' LAT'].tolist()
+lons = tract_data[' LON'].tolist()
+
+data = []
+topRight = []
+for p, g, t, la, lo in zip(populations, geoID, tractName, lats, lons):
+    if len(topRight) == 0 or (la > topRight[3] and lo > topRight[4]):
+        topRight = [g, p, t, la, lo]
+
+
+print(topRight)
+distances = []
+for p, g, t, la, lo in zip(populations, geoID, tractName, lats, lons):
+    d = math.sqrt((la - topRight[3])**2) + ((lo - topRight[4])**2)
+    distances.append(d)
+
+for p, g, t, la, lo, d in zip(populations, geoID, tractName, lats, lons, distances):
+    data.append([p, g, t, la, lo, d])
+
+data = sorted(data, key=lambda x: x[5])
+
+print(data)
+
+df = pd.DataFrame(data, columns=['GEOID', 'POP', 'FULL_NAME', 'LAT', 'LON', 'DISTANCE_TO_TOP_RIGHT'])
+df.to_csv("ordered_tracts.csv")
