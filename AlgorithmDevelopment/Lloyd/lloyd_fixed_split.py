1ddbc158989a09c7b4451a3988d24aab67868cbc
Tim Romer <Tim.Romer@gmail.com>
2020-05-07 10:52:01 -0400

    Added documentation for the lloyd algorithm

--- AlgorithmDevelopment/Lloyd/lloyd_fixed_split.py
+++ AlgorithmDevelopment/Lloyd/lloyd_fixed_split.py
@@ -6,10 +6,15 @@ import csv, os, json, numpy, random, math
 import pandas as pd
 from pprint import pprint
 
-#Globals
-filename = "ordered_tracts.csv"
-data = []
-distListKeys = ['d1','d2','d3','d4','d5','d6','d7','d8','d9','d10','d11','d12','d13','d14','d15','d16']
+# Globals
+filename = "ordered_tracts.csv" # File name on where to get initial district centers
+data = [] # Storing tract and district data
+# List of district names to write to the outputted json file
+distListKeys = ['d1','d2','d3','d4',
+                'd5','d6','d7','d8',
+                'd9','d10','d11','d12',
+                'd13','d14','d15','d16']
+# List to store the tracts in certain districts
 distList = {'d1':[],
             'd2':[],
             'd3':[],
@@ -32,8 +37,34 @@ distList = {'d1':[],
 TARGET_DISTRICT_MEAN = 710767
 TARGET_DISTRICT_SD   = TARGET_DISTRICT_MEAN + (TARGET_DISTRICT_MEAN * 0.005)
 
-#class
+# -------------------------- Tract Class --------------------------------------
+
 class tract:
+    """
+    A class used to represent a census tract
+
+    ...
+    Attributes
+    ----------
+    id : str
+        a unique string that represents each tract
+    pop : int
+        the population of the census tract
+    x : float
+        the longitude of the geographic center of the tract
+    y : float
+        the latitude of the geographic center of the tract
+
+    Methods
+    -------
+    getCoords()
+        returns the latitude and longitude in a numpy array
+    getPop()
+        returns the population for the tract
+    getId()
+        returns the unique id for the tract
+    """
+
     def __init__(self, id, pop, x, y):
         self.id = id
         self.pop = pop
@@ -53,7 +84,25 @@ class tract:
     def __repr__(self):
         return "{}, {}, {}".format(self.id, self.pop, self.coords)
 
+# ----------------------- Helper Functions ------------------------------------
+
 def transferData(results, data):
+    """
+    This function will transfer the results of the algorithm into a new form 
+    to write to the json output.
+
+    Parameters
+    ----------
+    results : Dictionary 
+        results of the algorithm
+    data : List
+        object to write results of the algorithm to
+
+    Returns
+    -------
+        Nothing as data is a global object
+    """
+
     for key in results.keys(): #key -> int
         for loc in results[key]: #loc -> numpy array
             for d in data: #d -> list
@@ -63,8 +112,21 @@ def transferData(results, data):
                     data.pop(data.index(d))
                     break
 
-#Find current total pop of specified district
 def totalPop(key):
+    """
+    Find current total pop of specified district
+    
+    Parameters
+    ----------
+    key : str
+        a district name to get the population of
+
+    Returns
+    -------
+    int
+        the total population for a specified district
+    """
+
     data = distList[key]
     totalPop = 0
     if len(data) == 0:
@@ -76,8 +138,11 @@ def totalPop(key):
     return totalPop
 
 
-#Print out pop of each districts and total pop
 def printPop():
+    """
+    Print out pop of each districts and total pop
+    """
+
     ohioPop = 0
     for key in distList.keys():
         ohioPop += totalPop(key)
@@ -85,26 +150,109 @@ def printPop():
 
     print('ohio pop = ', ohioPop)
 
+def print_cluster_pops(clusters):
+"""
+This is a helper method to help print the cluster populations
+
+Parameters
+----------
+Clusters : list
+    a list of district centers
+"""
+total_tracts = 0
+for c in clusters:
+    cluster_pop = 0
+    for index in clusters[c]:
+        cluster_pop += index.pop
+    total_tracts += len(clusters[c])
+    print(c, cluster_pop, len(clusters[c]))
+print(total_tracts)
+
+def read_initial_centers(X):
+"""
+This will read the initial centers from a csv with chosen centers
+
+Parameters
+----------
+X : List
+    List of tracts
+
+Returns
+-------
+list
+    initial districts
+
+"""
+initial = pd.read_csv('optimized_initial_centers.csv')
+ret = []
+for index, row in initial.iterrows():
+    for x in X:
+        if row['geoid'] == x.getId():
+            ret.append(x)
+return ret
+
 # ----------------------Algorithm----------------------------------------
 
-#input x - current data point
-#input
 def bestKey(input, mu, clusters):
+    """
+    Will find the closest availbale tract for a district
+
+    Parameters
+    ----------
+    input : tract
+        tract object to examine
+    mu : list
+        a list of the optimal geographic centers for districts
+    clusters : dictionary
+        dictionary of districts and the list of tracts that are in each district
+    """
+
     x = input.getCoords()
-    # minimum = min([(i[0], numpy.linalg.norm(x-mu[i[0]])) for i in enumerate(mu)])[0]
     bestkey = min([(i[0], numpy.linalg.norm(x-mu[i[0]])) for i in enumerate(mu)], key=lambda t:t[1])[0]
-    # print(minimum)
     return bestkey
 
-# Calculate the distance from the tract to the center
 def getDist(tract_coords, center_coords):
+    """
+    Calculate the distance from the tract to the center
+
+    Parameters
+    ----------
+    tract_coords : array
+        the geographic center of a tract
+    center_coords : array
+        the geographic center of a district
+
+    Returns
+    -------
+    float
+        The distance between the tract centers and district centers
+    """
+
 	return math.sqrt(((tract_coords[0] - center_coords[0])**2) + ((tract_coords[1] - center_coords[1])**2))
 
-# Will find the closest tract to the center provided
 def find_closest_tract(X, center):
-	min_dist  = 100000000000000 # Some arbitrary value that is larger than any possible distance
+    """
+    Will find the closest tract to the center provided
+    
+    Parameters
+    ----------
+    X : list
+        List of tracts to loop through
+    center : center
+        array of centers coordinates for the district 
+
+    Returns
+    -------
+    tract
+        closest tract to the center
+    """
+
+    # Some arbitrary value that is larger than any possible distance
+	min_dist  = 100000000000000
 	min_tract = X[0]
-	# Loop through all of the tracts to get find the closest tract to the center
+
+	# Loop through all of the tracts to get find the closest tract to the 
+    # center
 	for x in X:
 		distance = getDist(x.getCoords(), center)
 		if distance < min_dist:
@@ -112,8 +260,22 @@ def find_closest_tract(X, center):
 			min_tract = x
 	return min_tract
 
-
 def find_min_pop_cluster(clusters):
+    """
+    Will loop through the districts, calculate each population, then return the
+    center with the minimum population.
+
+    Parameters
+    ----------
+    clusters : list
+        A list of districts 
+
+    Returns
+    -------
+    int
+        The index of the minimum district
+    """
+
     min_cluster_index = -1
     min_pop = 100000000000000
     for c in clusters:
@@ -124,9 +286,26 @@ def find_min_pop_cluster(clusters):
             min_pop = cluster_pop
             min_cluster_index = c
     return min_cluster_index
-# This function will assign all of the tracts to a district round robin style
-# until all tracts are assigned
+
+
 def cluster_points(X, mu):
+    """
+    This function will assign all of the tracts to a district round robin style
+    until all tracts are assigned
+
+    Parameters
+    ----------
+    X : list
+        List of tracts
+    mu : list
+        List of district centers
+
+    Returns
+    -------
+    dictionary
+        dictionary of districts and the tracts inside each district
+    """
+
     center_count = 0
     clusters = {} # The object being returned
     taken_tracts = [] # Tracking the tracts that have been added
@@ -159,11 +338,23 @@ def cluster_points(X, mu):
                 center_count += 1
     return clusters
 
-#Creates new centers
-#input: mu - list of current centers
-#input: cluster - list of current clusters
-#output: newmu list of new centers
 def reevaluate_centers(mu, clusters):
+    """
+    Creates new centers based on the districts that have been created. Apart
+    of the iterative process.
+
+    Parameters
+    ----------
+    mu : list
+        list of current centers
+    cluster : dictionary
+        list of current clusters
+
+    Returns
+    -------
+    list
+        A list of new/optimized centers
+    """
     newmu = []
     keys = sorted(clusters.keys())
 
@@ -175,41 +366,47 @@ def reevaluate_centers(mu, clusters):
         newmu.append(numpy.mean(clusterCoords, axis = 0))
     return newmu
 
-#Tuple is a collection with UNCHANGEABLE order
-#This checks if the current mu's are the same
+
 #input: mu - current centers
 #input: oldmu - old centers
 #output: true if tuples are same, false otherwise
 def has_converged(mu, oldmu):
+    """
+    Tuple is a collection with UNCHANGEABLE order
+    This checks if the current mu's are the same
+
+    Parameters
+    ----------
+    mu : list
+        List of the new optimized centers
+    oldmu : list
+        List of the previous centers
+
+    Returns
+    -------
+    bool
+        wether convergence has been reached. All oldmu is equal to mu
+    """
+
     return set([tuple(a) for a in mu]) == set([tuple(a) for a in oldmu])
 
-# This is a helper method to help print the cluster populations
-def print_cluster_pops(clusters):
-    total_tracts = 0
-    for c in clusters:
-        cluster_pop = 0
-        for index in clusters[c]:
-            cluster_pop += index.pop
-        total_tracts += len(clusters[c])
-        print(c, cluster_pop, len(clusters[c]))
-    print(total_tracts)
-    exit()    
 
-def read_initial_centers(X):
-    initial = pd.read_csv('optimized_initial_centers.csv')
-    ret = []
-    for index, row in initial.iterrows():
-        for x in X:
-            if row['geoid'] == x.getId():
-                ret.append(x)
-    return ret
 
-#Call this function for Lloyds
-#X is a list of all points, K is the number of clusters you want
-#output: dictionarry of all clusters
-#oldmu is a list of the old find_centers
-#mu is a list of the current centers
+
 def find_centers(X, K):
+    """
+    Call this function for Lloyds
+    X is a list of all points, K is the number of clusters you want
+    output: dictionarry of all clusters
+    oldmu is a list of the old find_centers
+    mu is a list of the current centers
+
+    Driver function for lloyds. Will check convergence for district centers
+    and will add all tracts to the districts. Right now, this method will
+    only run once because the initial district centers were chosen and will
+    not change.
+    """
+
     # Initialize to K random centers
     oldmuTracts = random.sample(X, K)
     muTracts = read_initial_centers(X)
@@ -219,12 +416,11 @@ def find_centers(X, K):
     clusters = {}
     ogX = X.copy()
 
-    # TIM: get the coordinates for each tract sample and append to mu/oldmu
+    # get the coordinates for each tract sample and append to mu/oldmu
     for i in range(K):
         mu.append(muTracts[i].getCoords())
         oldmu.append(oldmuTracts[i].getCoords())
 
-    # TIM: Why are we checking for convergence?
     while not has_converged(mu, oldmu):
         count += 1
         oldmu = mu
@@ -243,9 +439,12 @@ def find_centers(X, K):
     print('Converged on iteration: ', count)
     return(clusters)
 
-#creates list of data points based on the census tracts
-#x and y values are the coordinates
+
 def init_data(data):
+    """
+    creates list of data points based on the census tracts
+    x and y values are the coordinates
+    """
     output = list()
     for d in data:
         temp = tract(int(d[1]), int(d[2]), float(d[4]), float(d[5]))
